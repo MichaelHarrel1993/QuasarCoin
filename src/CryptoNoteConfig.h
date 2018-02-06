e93aee2b0378e827e5ff76259861bf18238f299e
Michael <33591343+MichaelHarrel1993@users.noreply.github.com>
2018-02-06 18:46:35 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -123,8 +123,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  12583;
-const int      RPC_DEFAULT_PORT                              =  12584;
+const int      P2P_DEFAULT_PORT                              =  13583;
+const int      RPC_DEFAULT_PORT                              =  13584;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -141,7 +141,7 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-const char* const SEED_NODES[] = { "45.76.131.122:12583","198.54.124.187:12583" };
+const char* const SEED_NODES[] = { "45.76.131.122:13583","198.54.124.187:13583" };
 
 
 struct CheckpointData {
